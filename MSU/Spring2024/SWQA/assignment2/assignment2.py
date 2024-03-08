1177b8a6f36f143e7c22f47843c7e898a1f9dac0
Connor Carr-Laughlin <cjclmemphis@gmail.com>
2024-03-07 21:37:30 -0600

    BMI Calculator with Tests

---
+++ MSU/Spring2024/SWQA/assignment2/assignment2.py
@@ -0,0 +1,71 @@
+def bmi(weight, height):
+    try:
+        weight = int(weight)
+        height = int(height)
+    except ValueError:
+        raise TypeError('Invalid input. Please enter a number.')
+    weightKG = weight / 2.205
+    heightM = height / 39.37
+    bmi = weightKG / (heightM ** 2)
+    bmi = round(bmi, 1)
+    return bmi
+
+def classifyBMI(bmi):
+    if type(bmi) != float:
+        raise TypeError('Invalid input. BMI must be a number.')
+    
+    if bmi < 18.5:
+        return 'Underweight'
+    elif bmi >= 18.5 and bmi <= 24.9:
+        return 'Normal weight'
+    elif bmi >= 25 and bmi <= 29.9:
+        return 'Overweight'
+    elif bmi >= 30:
+        return 'Obese'
+    
+    
+def main():
+    while True:
+        try:
+            heightFt = int(input('Enter your height in feet: '))
+            heightIn = int(input('Enter your height in inches: '))
+            height = (heightFt * 12) + heightIn
+            break
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+
+    while True:
+        try:
+            weight = int(input('Enter your weight in pounds: '))
+            break
+        except ValueError:
+            print('Invalid input. Please enter a number.')
+    
+    result = bmi(weight, height)
+    
+    classification = classifyBMI(result)
+
+    print('Your BMI is:', result)
+    
+    print('You are:', classification)
+    
+    
+    while True:
+        try:
+            answer = input('Would you like to run the program again? (yes/no): ')
+            if answer == 'yes':
+                main()
+            elif answer == 'no':
+                print('Goodbye')
+                break
+            else:
+                print('Invalid input. Please enter yes or no.')
+        except ValueError:
+            print('Invalid input. Please enter yes or no.')
+                
+                
+                
+if __name__ == '__main__':
+    main()
+
+
---
+++ MSU/Spring2024/SWQA/assignment2/test_assignment2.py
@@ -0,0 +1,35 @@
+import pytest
+from assignment2.assignment2 import bmi, classifyBMI
+
+
+def test_bmi_invalid_input():
+    with pytest.raises(TypeError):
+        bmi('weight', 60)
+    with pytest.raises(TypeError):
+        bmi(150, 'height')
+    with pytest.raises(TypeError):
+        bmi('weight', 'height')
+
+def test_bmi():
+    assert bmi(150, 60) == 29.3
+
+    
+def test_classifyBMI_EPC():
+    assert classifyBMI(18.4) == 'Underweight'
+    assert classifyBMI(18.5) == 'Normal weight'
+    assert classifyBMI(18.6) == 'Normal weight'
+    assert classifyBMI(24.8) == 'Normal weight'
+    assert classifyBMI(24.9) == 'Normal weight'
+    assert classifyBMI(25.0) == 'Overweight'
+    assert classifyBMI(25.1) == 'Overweight'
+    assert classifyBMI(29.8) == 'Overweight'
+    assert classifyBMI(29.9) == 'Overweight'
+    assert classifyBMI(30.0) == 'Obese'
+    assert classifyBMI(30.1) == 'Obese'
+    
+def test_classifyBMI_invalid_input():
+    with pytest.raises(TypeError):
+        classifyBMI('bmi')
+    with pytest.raises(TypeError):
+        classifyBMI(18)
+    
